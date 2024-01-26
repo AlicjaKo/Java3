c877fa94634120a117a8877ec5b41e5824de2a2c
Ala <ala@Alas-MacBook-Pro.local>
2024-01-26 10:25:18 +0200

    EX 1

--- e01/src/main/java/exercise/App.java
+++ e01/src/main/java/exercise/App.java
@@ -1,6 +1,6 @@
 package exercise;
 
-// TODO
+
 
 public class App {
     private App() {
@@ -8,6 +8,84 @@ public class App {
     }
 
     public static void main(String[] args) {
+        Square s = new Square(4);
+        s.setSideLength(5);
+        System.out.println("Area of the square: "+s.getArea());
+        System.out.println("Perimeter of the square: "+s.getPerimeter());
 
+        Circle c = new Circle(4);
+        c.setRadius(4);
+        System.out.println("Area of the circle: "+c.getArea());
+        System.out.println("Perimeter of the circle"+c.getPerimeter());
+    
+    
+    }
+}
+
+class Shape {
+    //paramether
+    private int numberOfSides;
+
+    //constructor
+    public Shape() {
+
+    }
+    public Shape(int n) {
+        this.numberOfSides = n;
+    }
+
+    //methods
+    public void setNumberOfSides(int n) {
+        this.numberOfSides = n;
+    }
+    public int getNumberOfSides() {
+        return this.numberOfSides;
+    }
+}
+
+class Square extends Shape {
+    //paramether
+    private int sideLength;
+
+    public Square(int side) {
+        this.sideLength = side;
+    }
+
+    //get/set
+    public void setSideLength(int i) {
+        this.sideLength = i;
+    }
+    public int getSideLength() {
+        return this.sideLength;
+    }
+
+    //methods
+    public int getArea() {
+        return this.sideLength*this.sideLength;
+    }
+    public int getPerimeter() {
+        return 4*this.sideLength;
+    }
+}
+
+class Circle extends Shape {
+    private int radius;
+
+    public Circle(int rad){
+        this.radius = rad;
+    }
+
+    public void setRadius(int r) {
+        this.radius = r;
+    }
+    public int getRadius() {
+        return this.radius;
+    }
+
+    public int getArea () {
+        return  (int)(Math.PI*this.radius*this.radius);
+    }
+    public int getPerimeter () {
+        return (int)(Math.PI*this.radius*2);
     }
 }
